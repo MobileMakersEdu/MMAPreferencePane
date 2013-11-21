e0cfa9a07b00d49adb451a4b035cfbaf96d9528a
Max Howell <mxcl@me.com>
2013-11-21 11:54:48 -0600

    Fix typo in GitX URL

--- diagnostics/MMGitXDiagnostic.m
+++ diagnostics/MMGitXDiagnostic.m
@@ -8,8 +8,8 @@
         return YES;
 
     id info = @{
-        NSLocalizedDescriptionKey: @"You need to install GitX",
-        NSLocalizedRecoverySuggestionErrorKey: @"http://rowanj.github.io"
+        NSLocalizedDescriptionKey: @"You need to install GitX (after downloading, make sure to drag and drop the application to your /Applications folder!)",
+        NSLocalizedRecoverySuggestionErrorKey: @"http://rowanj.github.io/gitx/"
     };
     *error = [NSError errorWithDomain:MMErrorDomain code:MMDiagnosticFailedAmber userInfo:info];
     return NO;
