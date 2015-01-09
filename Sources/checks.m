4b8fc5e91c519f93cafbe996c86cda74510b815f
Max Howell <mxcl@me.com>
2015-01-09 12:28:23 -0600

    Actually check for git user and email

--- Sources/checks.m
+++ Sources/checks.m
@@ -78,7 +78,10 @@ Promise *MMACheckGitHub() {
         }
     });
 
-    id p2 = [NSTask:@"/usr/bin/git config --global user.name"].promise.catch(^{
+    id p2 = [NSTask:@"/usr/bin/git config --global user.name"].promise.then(^(NSString *stdout){
+        if (!stdout.chuzzle)
+            @throw @1;
+    }).catch(^{
         id info = @{
             NSLocalizedDescriptionKey: @"You need to set your git username",
             NSLocalizedRecoverySuggestionErrorKey: @"http://help.github.com/articles/setting-your-username-in-git"
@@ -86,7 +89,10 @@ Promise *MMACheckGitHub() {
         return [NSError errorWithDomain:MMAErrorDomain code:MMADiagnosticFailedRed userInfo:info];
     });
 
-    id p3 = [NSTask:@"/usr/bin/git config --global user.email"].promise.catch(^{
+    id p3 = [NSTask:@"/usr/bin/git config --global user.email"].promise.then(^(NSString *stdout){
+        if (!stdout.chuzzle)
+            @throw @1;
+    }).catch(^{
         id info = @{
             NSLocalizedDescriptionKey: @"You’ve set your git username, but you also need to set your git email, USE THE SAME EMAIL AS YOUR GITHUB ACCOUNT",
             NSLocalizedRecoverySuggestionErrorKey: @"http://help.github.com/articles/setting-your-email-in-git"
